ccadae5a13ecef70a54eedccb8d1d339619c8c3d
mouheb htiwch <93223049+Mouuheb@users.noreply.github.com>
2024-08-27 13:01:38 +0100

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ function App() {
       <Stars/>
       <div className='s2' >
         <div className='title'>
-          <h1 className='metal-mania-regular' >SPASE</h1>
+          <h1 className='metal-mania-regular' >SPACE</h1>
+          <h5>Simple 3D Earth with react.js</h5>
         </div>
         <div className='obj'>
           <Earth/>
